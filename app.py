6c7c025e5fa6cf3606d08703428936823daa162f
Siddhi Rajole <siddhi1375rajole@gmail.com>
2025-10-30 15:04:22 +0530

    Connected MySQL Clever Cloud DB

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-app.config['MYSQL_CURSORCLASS'] = os.getenv('MYSQL_CURSORCLASS')
-app.secret_key = os.getenv('SECRET_KEY')
-
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
 mysql = MySQL(app)
+
+
 
 # ------------------- DECORATOR FOR PROTECTED ROUTES -----------------
 def login_required(f):
